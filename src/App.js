1165bd85e9776922f1cbb04ee54455ea53eda752
Volodymyr <volodymyrquo@gmail.com>
2020-11-09 07:48:15 +0200

    add div for a city

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 
 const App = (props) => {
   const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
   const search = async (e) => {
     if (e.key === "Enter") {
       const data = await fetchWeather(query);
-      console.log(data);
+      setWeather(data);
+      setQuery("");
     }
   };
 
@@ -24,6 +26,14 @@ const App = (props) => {
         }}
         onKeyPress={search}
       />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+        </div>
+      )}
     </div>
   );
 };
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -4,7 +4,6 @@ const URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = "b00f4091a7adbf1936008eb7f5ad0fe8";
 
 export const fetchWeather = async (query) => {
-  debugger;
   const { data } = await axios.get(URL, {
     params: { q: query, units: "metric", appid: API_KEY },
   });
