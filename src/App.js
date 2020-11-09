82dc63efb3b141d2e36b3fcc78fbb3a3603a9564
Volodymyr <volodymyrquo@gmail.com>
2020-11-09 08:13:02 +0200

    finished react app

--- src/App.js
+++ src/App.js
@@ -32,6 +32,18 @@ const App = (props) => {
             <span>{weather.name}</span>
             <sup>{weather.sys.country}</sup>
           </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
         </div>
       )}
     </div>
